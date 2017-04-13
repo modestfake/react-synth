54efcbedd88c029716d3312aa77007380a83f247
Vitali Saienko <modestfake@gmail.com>
2017-04-13 17:11:11 +0300

    Add basic mixer elements

--- src/blocks/Mixer.js
+++ src/blocks/Mixer.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import Fader from '../controls/Fader';
 
 class Mixer extends Component {
   render() {
     return (
       <div className="mixer">
         <h4 className="block_header">Mixer</h4>
+        <div className="mixer_wrapper">
+          <div className="mix_parts">
+            <div className="ruler st-left"></div>
+            <Fader type="MIX A" defaultValue="50" />
+            <div className="ruler st-between"></div>
+            <Fader type="MIX B" defaultValue="50" />
+            <div className="ruler st-right"></div>
+          </div>
+
+          <div className="mix_master">
+            <div className="ruler st-left"></div>
+            <Fader type="master" defaultValue="100" />
+            <div className="ruler st-right"></div>
+          </div>
+
+          <div className="mix_meter"></div>
+        </div>
       </div>
     );
   }
--- src/controls/Fader.js
+++ src/controls/Fader.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Fader extends Component {
   render() {
     return (
-      <div className="fader">
+      <div className={'fader' + (this.props.type === 'master' ? ' master' : '')}>
         <label>{this.props.type}</label>
         <input type="range" defaultValue={this.props.defaultValue} />
       </div>
